d96bcd2745fe0aca139644763476a8f1ba4fcb26
yuseirf <yusei.tanaka2112325@icloud.com>
2022-10-21 01:23:44 +0900

    import bootstrap

--- src/index.tsx
+++ src/index.tsx
@@ -3,5 +3,6 @@ import { render } from 'solid-js/web';
 
 import './index.css';
 import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 render(() => <App />, document.getElementById('root') as HTMLElement);
